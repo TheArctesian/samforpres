12caa2335a55ccfea8e613f084481bdfae6fb35b
The Arctesian <87273292+TheArctesian@users.noreply.github.com>
2024-03-15 19:36:11 -0700

    style changes

---
+++ src/routes/Policy/Career/+page.svelte
@@ -0,0 +1,141 @@
+<div>
+	<h1>Career Opportunity Enhancement Program (COEP):</h1>
+
+	<h2>Contextualizing the Issue:</h2>
+
+	<p>
+		Despiting holding its place as the number one public university in the country, Berkeley
+		ill-equips its students for entering the workforce. In the class of 2023, <a href="https://career.berkeley.edu/start-exploring/where-do-cal-grads-go/">25% of graduates are
+		still looking for jobs,</a> which is a staggering amount in comparison to other equivalent campuses
+		such as <a href="https://gitnux.org/ucla-employment-statistics/">UCLA</a>, <u>where the same number is 7%</u>. Similarly, 20% of Berkeley's graduates pursue
+		advanced studies in graduate, law, medical schools but are compelled to independently prepare
+		for the entrance exams, often incurring significant personal expenses on preparatory courses.
+	</p>
+
+	<p>
+		Our career readiness programs must improve. ASUC has taken strides to address this need, like
+		purchasing LinkedIn premium memberships for 6% of overall students, but this is simply not
+		enough. This solution cost $158,000 for just 2000 memberships and only for 6 months, which if
+		implemented for an entire calendar year for all of the undergraduate classes, would cost an
+		unreasonable $7.1 million.
+	</p>
+
+	<p>
+		Berkeley’s Career Engagement Center (CEC) offers extensive counseling, advising, training, and
+		more to undergraduates. However, as demonstrated by aforementioned statistics, it is likely that
+		the resources provided by the CEC are being underused, and Berkeley students still need further
+		support. We propose the following improvements to our Career preparation programs:
+	</p>
+
+	<h3>1. Enhanced Alumni networking</h3>
+
+	<p>
+		The largest difference found with UCLA’s and other universities’ career preparation programs and
+		ours is our Alumni networking program. Whereas UCLA has an extensive alumni website, showing all
+		alumni with the availability to advise on job seeking, hire students, or offer advice at any
+		point in students career path; We have no equivalent.
+	</p>
+
+	<p>
+		Our alumni portal is clunky and does not show any indication of which alumni may be willing to
+		help students obtain positions at their company, or even the ones who are willing to talk to
+		current students. The user interface is quite outdated and inefficient, giving current students
+		very limited information on any alumni. We propose that the ASUC invests substantial funding in
+		rebuilding our Alumni portal to be similar to that of UCLA’s, which would greatly benefit those
+		looking to network within the Berkeley community. Additionally, Handshake is rife with scam job
+		postings, and an internal, alumni based job board would mitigate such issues.
+	</p>
+	<p>
+		For those who have already graduated, the CEC offers continued advising for 2 years through
+		Handshake. When you browse these advising appointments though, you will find that they are only
+		for specific majors, completely ignoring the majority of L&S, Arts, and Humanities majors. We
+		propose that the ASUC encourages the CEC to expand their post-graduation advising to include
+		these currently unserved majors.
+	</p>
+	<h3>2. Targeted outreach to underserved communities</h3>
+
+	<p>
+		Although the CEC offers great opportunities and support for those in underserved communities,
+		more students in these communities must be made aware of the existence of such programs. We
+		propose that the ASUC forms a committee on student career outreach to address this problem. We
+		propose two immediate action items:
+	</p>
+
+	<p>
+		The CEC partners with campus affinity groups of these underrepresented groups and hosts open
+		workshops to broadcast the work the CEC offers for students in such groups.
+	</p>
+
+	<p>
+		The ASUC invests in campus advertising for the CEC, specifically targeting these
+		underrepresented communities.
+	</p>
+	<h3>3. MCAT / LSAT preparation</h3>
+
+	<p>
+		Although Berkeley boasts impressive matriculation and acceptance rates for students continuing
+		onto medical and law school, the preparation courses for the required standardized testing is
+		often extremely expensive, costing students up to $4000. We propose that the ASUC allocates a
+		substantial amount of funding to subsidizing the preparation materials for students. Though this
+		is a costly endeavor, we believe that this is a much more effective cause to spend $158,000,
+		compared to purchasing LinkedIn premium memberships for a fractional amount of students.
+	</p>
+	<h3>
+		4. Workshops on emerging technologies and new skills that are crucial in the current job market
+	</h3>
+
+	<p>
+		As job markets get more difficult to navigate, the skills needed to obtain jobs change. We
+		propose the creation of a task force that annually reviews new job skill trends, collaborates
+		with the CEC, and hosts workshops for students on obtaining these new skills. Equipping our
+		students with the most current skills will allow our students to survive.
+	</p>
+	<h3>5. Linkage Networks with Local Companies and Corporations</h3>
+
+	<p>
+		In its admissions materials, UC Berkeley boasts its loci within the Bay Area as a major selling
+		point for prospective applicants. Along with Stanford University, UC Berkeley is the top
+		university in the region. Nevertheless, students are not given resources or opportunities to
+		reach major engineering, tech, and biotech networks. While campus offers job fairs and
+		internship workshops, there are few positions available and primarily depend upon an excess of
+		individual effort. Even using the current private internship system through Berkeley Discovery’s
+		Student Opportunity Center, internships are not designated for campus students specifically. We
+		propose working with major companies such as Genentech, Microsoft, Nvidia, and others to build
+		positions for Berkeley student interns within the companies. This solution is not unique, in the
+		Bay Area, Stanford University places students at top companies. Extrapolating to public
+		universities, many colleges across the United States offer co-op programs and linked companies
+		that offer internships to partner universities.
+	</p>
+</div>
+
+<style>
+	div {
+		padding-left: 2rem;
+		padding-right: 2rem;
+	}
+	h1 {
+		color: var(--pri);
+		font-size: 5vw;
+		text-align: center;
+	}
+	h2 {
+		color: var(--pri);
+		font-size: 3vw;
+	}
+	p {
+		margin-top: 1rem;
+	}
+	h3 {
+		margin-top: 1rem;
+		font-size: 2vw;
+		color: var(--sec);
+		text-decoration-line: underline;
+	}
+	li {
+		margin-left: 1rem;
+	}
+	a {
+		color: var(--sec);
+		text-decoration-line: underline;
+	}
+</style>
