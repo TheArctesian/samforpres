2b5f3bc0347652c4423d050e602ebaaab97cc0ca
The Arctesian <87273292+TheArctesian@users.noreply.github.com>
2024-03-15 18:24:57 -0700

    added health

---
+++ src/routes/Policy/Health/+page.svelte
@@ -0,0 +1,301 @@
+<script>
+	import '../../../app.css';
+</script>
+
+<div>
+	<h1 class="text-center">Student Health</h1>
+
+	<p class="">
+		Student health and wellbeing is paramount to the success, happiness, and wellbeing of our
+		students. We’ve identified substantial problems in our mental health services, and our
+		universities current work on Sexual Violence and Sexual Harrasement (SVSH). We propose extensive
+		policy proposals to address both of these issues.
+	</p>
+
+	<h2>Mental Health:</h2>
+
+	<p>
+		Counseling and Psychological Services (CAPS) at UC Berkeley has a mission to provide
+		comprehensive support for students' emotional, psychological, educational, social, and cultural
+		development. Despite this mission, there have been notable shortcomings in accessibility and
+		effectiveness. Historically, CAPS offered walk-in services for career counseling, but this has
+		transitioned to scheduled appointments, causing inconvenience for students. The Career
+		Counseling Library, once readily accessible, now operates virtually with limited availability.
+		Individual meetings, crucial for personalized support, require 24-hour advance scheduling,
+		hindering immediate assistance. Furthermore, drop-in consultations, though labeled as such,
+		necessitate appointments made within the week, constraining timely access. With a meager count
+		of 26 counselors for a vast student population, long waiting periods for counseling are
+		commonplace. Additionally, the availability of therapy sessions, both virtual and in-person,
+		remains limited, leaving students with constrained options and long wait times. Group
+		counseling, while offered, demands significant effort from students to secure participation,
+		with no guarantee of acceptance. The process for couples counseling is similarly burdensome,
+		with ambiguous timelines for response and scheduling. Despite efforts to provide support through
+		various channels, CAPS' inefficiencies in accessibility and responsiveness have resulted in
+		significant challenges for students seeking mental health services.
+	</p>
+
+	<p>
+		For students seeking long-term therapy, CAPS offers resources such as the referral database,
+		containing names of clinicians interested in working with UC Berkeley students. This database
+		allows students to search for providers based on their insurance plan. Additionally, SHIP
+		members have access to Teladoc, enabling video visits with licensed therapists or psychologists
+		from anywhere. Students with SHIP insurance can also utilize Wellfleet Member Services or the
+		Blue Shield of California PPO network to locate in-network providers. UHS Counselors assist
+		students in finding off-campus providers compatible with their healthcare plan. Those with other
+		insurance plans can use their provider's resources to locate counseling services and utilize the
+		CAPS referral database as needed. Despite these resources, challenges in accessing affordable
+		and appropriate long-term therapy persist for students at UC Berkeley. In addition, it's
+		important to note the financial challenges faced by mental health providers in collaborating
+		with SHIP and its partners. According to Weksler, UC Berkeley's current and past student medical
+		insurance partners, including Anthem and Aetna, have only reimbursed providers around 50 percent
+		of their regular full fee. With the transition to Blue Shield, providers anticipate even lower
+		reimbursement rates, making it financially unfeasible for many to offer more than one or two
+		therapy slots per week for students, especially considering the high cost of living in the area.
+		This financial strain further exacerbates the existing difficulties in accessing timely and
+		affordable mental health care for UC Berkeley students.
+	</p>
+
+	<p>There is so much that can be done. All we need is your support.</p>
+
+	<h2>Background:</h2>
+
+	<p>
+		The Counseling and Psychological Services (CAPS) at UC Berkeley aims to provide comprehensive
+		support for students' emotional, psychological, educational, social, and cultural development.
+		However, despite this mission, there have been notable shortcomings in accessibility and
+		effectiveness.
+	</p>
+
+	<h3>Different Counseling Types:</h3>
+	<li>
+		Career Counseling: Historically offered as walk-in services, now transitioned to scheduled
+		appointments, causing inconvenience for students. The Career Counseling Library operates
+		virtually with limited availability.
+	</li>
+	<li>
+		Individual Counseling: Requires 24-hour advance scheduling. Drop-in consultations necessitate
+		appointments made within the week, constraining timely access.
+	</li>
+	<li>
+		Group Counseling: Requires significant effort to secure participation, with no guarantee of
+		acceptance.
+	</li>
+	<li>
+		Couples Counseling: Burdensome process with no clear timelines for response and scheduling
+	</li>
+	<h3>Issues Present:</h3>
+	<li>
+		Limited Availability: Only 26 counselors for a vast student population, resulting in long
+		waiting periods for individual and couples counseling.
+	</li>
+	<li>
+		Limited Therapy Sessions: Both virtual and in-person sessions remain limited, leading to
+		constrained options and long wait times.
+	</li>
+	<li>
+		Effort-Intensive Processes: Group and couples counseling demand significant effort from
+		students, with no guarantee of acceptance and ambiguous timelines.
+	</li>
+	<li>
+		Financial Strain on Providers: Mental health providers collaborating with UC Berkeley's Student
+		Health Insurance Plan (SHIP) and its partners face financial challenges due to low reimbursement
+		rates, making it financially unfeasible for many to offer more than one or two therapy slots per
+		week.
+	</li>
+
+	<h2>Objectives:</h2>
+	<li>Improve accessibility and responsiveness of CAPS services.</li>
+	<li>Increase the availability of all counseling sessions.</li>
+	<li>Simplify the process for accessing all counseling services</li>
+	<li>
+		Address the financial strain on mental health providers to ensure adequate therapy slots for
+		students.
+	</li>
+
+	<h2>Policy Recommendations:</h2>
+	<li>
+		Increase Staffing: Hire additional counselors to reduce waiting periods and increase
+		availability.
+	</li>
+	<li>
+		Streamline Appointment Processes: Simplify scheduling procedures for counseling to enhance
+		accessibility.
+	</li>
+	<li>
+		Expand Therapy Options: Increase the number of therapy sessions, both virtual and in-person, to
+		accommodate needs of students.
+	</li>
+	<li>
+		Enhance Collaboration with Providers: Negotiate higher reimbursement rates with insurance
+		partners to alleviate financial strain on mental health providers.
+	</li>
+	<li>
+		Improve Communication: Provide clear guidelines and timelines for students seeking counseling to
+		reduce ambiguity and effort.
+	</li>
+	<li>
+		Enhance Referral Services: Improve the CAPS referral database and coordination with off-campus
+		providers to facilitate access to long-term therapy options.
+	</li>
+	<li>
+		Advocate for Policy Changes: Advocate for policy changes at the university and state level to
+		prioritize mental health funding and support for students.
+	</li>
+	<h2>Counseling and Psychological Services (CAPS):</h2>
+	<h3>Overview of CAPS:</h3>
+	<div>
+		<p>
+			The mission of Counseling and Psychological Services (CAPS) is to support the emotional,
+			psychological, educational, social and cultural development of all UC Berkeley students
+			through a wide range of multiculturally based counseling, psychiatric, career consultation,
+			training and educational services. (Mission Statement)
+		</p>
+		<p>
+			Additionally, using data from a recent publication, when students are faced with the question:
+			“Within the last 6 months, do you feel like any kinds of things have been traumatic or very
+			difficult to handle?” the three most popular types of difficulty reported are “Academics”
+			(76.9%), “Career-related Issue” (50.7%), and “Intimate relationship” (36.7%).
+		</p>
+	</div>
+<p>	CAPS provides free counseling for students regardless of their insurance plan. All the resources provided require students to plan in advance.</p>
+	<br>
+	 <li> Career counseling assessment and library </li>
+	 <div>
+	 	<li> The Career Counseling Library is a unique collection of books, files, directories, and electronic resources staffed by a professional staff who will help you explore majors, careers, and graduate programs. </li>
+	 	<li> Students used to be able to just walk into the Career counseling and assessment library on a first come first serve basis, whenever they needed help, and talk to people who would provide information on career-related issues. However, the library has moved to the third floor of Tang Center and offers virtual programs</li>
+	 	<li> Students can also set up individual meetings that have to be scheduled <u>24 hours in advance.</u> During these meetings, students take the Meyers-Briggs personality test and the Strong Interest Inventory test. The results are then presented to a counselor who evaluates </li>
+	 </div>
+	<li>Drop-in consultations (UHS Mental Health Counselor) </li>
+	<div>
+		<li>Does not constitute mental health treatment</li>
+		<li>Are actually not “drop-in”. You still have to make appointments that are for that week. * There are a total of 26 counselors– a very small group of people compared to how big the student population is. </li></div>
+	<li>Individual and Couples counseling</li>
+	<div>
+	    <li>Same-day individual counseling </li>
+		<div>
+			<li>Appointments are released for scheduling each day and up to 16 hours ahead.  </li>
+			<li>Call CAPS or access eTang in the morning, or access eTang the night before for the next day. </li>
+			<li>Call CAPS when urgent support is needed - more appointment options are available by calling the main office at 511-642-9494 (M-F 9am-5pm).  </li>
+</div>
+			<li>Virtual Appointments with humanest</li>
+			<div>
+			<li>Drop in groups </li>
+			<div>
+				* Online– you join other people in workshops about journaling, yoga, mindfulness exercises, etc. 
+</div>
+			<li>Individual therapy sessions </li>
+			<div>
+					<li>Only 14 counselor </li>
+					<li>Times available for the next 1-3 weeks (limited times) </li>
+</div>
+				<li>NEST through humanest </li>
+				<div>
+					<li>Text-based expert-moderated peer forums </li>
+					<li>Counselor-led psychoeducation courses   </li>
+					<li>Drop in video groups                    </li>
+</div>
+		<li>Couples Counseling </li>
+		<div>
+			<li>Call (510) 642-9494 to make a couples counseling appointment <b>(couples counseling appointments cannot be made on eTang)</b>.  </li>
+			<li>Our staff will send you some pre-appointment forms to complete and once you send those back we can schedule your couples counseling appointment. </li>
+			<li>There is no exact time of when you will receive an answer </li>
+</div>
+</div>
+	</div>
+	<li>Group Counseling </li>
+	<div>
+		<li>Different workshops and programs listed throughout the week </li>
+
+	<div>
+			<li>Students have to either call and ask to be a part of the programs, send an email to the group facilitator or get a referral from a UHS provider </li>
+
+	<div>
+				<li>Again, there is no guarantee. A lot of effort has to be put into even being a part of these groups and you have to wait to see if the facilitators will reach out or not. </li>
+
+</div>
+</div>
+</div>
+	<li>Long Term Therapy </li>
+	<div>
+		<li>CAPS referral database</li>
+		<div>
+			<li>This searchable database contains names of clinicians who have expressed interest in working with UC Berkeley students. You may search by insurance to see providers who accept your insurance plan. At any time if you need help with referrals, you are welcome to request assistance from a CAPS counselor. Teladoc - Teladoc® allows SHIP members 18 and older to have a video visit with a licensed therapist or psychologist from the convenience of their home, their dorm, or on the go. </li>
+			</div>
+		<li> Students with SHIP</li>
+		<div>
+			<li> Through your Insurance - You may also locate in-network providers of your choice by calling Wellfleet Member Services at (877) 657-5033, case management support at (888) 893-7264, or by using Wellfleet's <u>provider search tool</u></li>
+			<li> Students with SHIP insurance also have access to the Blue Shield of California PPO network at the same rates. You can search for providers on their provider search tool. </li>
+			<li> When prompted to select a plan, choose “Blue Shield of California PPO.” </li>
+			<li> Work with a UHS Counselor - If you are currently connected with a UHS mental health provider (CAPS, Social Services, or Behavioral Health Provider), they can work with you to locate an off-campus provider that is maximally accessible, appropriate, and affordable with your health care plan.</li>
+</div>
+		<li>Students with other insurance </li>
+		<div> <li>Go through their provider to find counseling and using the database. </li></div>
+			</div>
+
+<h2>Sexual Safety: </h2>
+<h3>Context: </h3>
+<p>In recent years, there has been a problematic increase in sexual violence and sexual harassment on the UC Berkeley campus- a problem which has largely gone unaddressed by ASUC and admin at large. </p>
+<p>Stories about sexual assault cases going
+	into the bureaucratic void are only increasing, and it is clear that there is a distinct lack of transparency
+	that inflicts unneeded stress on already overwhelmed victims. In addition, victims deal with the same
+	problems with counseling: meetings at most every 2 weeks, when their needs need to be addressed not
+	only fast but consistently.  </p>
+<p>
+	The UCPD website says the increase in sexual violence reports is likely
+	inflation due to the return to in person instruction, and in addition that the increase “reflects individuals
+	feeling more confident in reporting incidents of SVSH, not necessarily an increase in SVSH incidents
+	occurring”. This is dismissive of the problem at large. </p>
+	
+	<p>And regardless of the truth of this statement,
+	597 SVSH reports were filed in 2022-23 at just the Office for the Prevention of Harassment and Discrimination,
+	which is an increase from 471 in 2021-22, 272 in 2020-21, and 446 in 2017-18. Change needs to be made,
+	but as of late there is nothing being done. Looking at even the most recent historical actions done
+	in the past, it is immediately striking that not a single historical- and thus substantive- change
+	has been done since 2017 (https://svsh.berkeley.edu/historical-campus-actions). The last facebook post
+	from the ASUC Sexual Violence Commission was in 2021. </p>
+	
+	<p>So nothing has been done yet the number rises.</p>
+<h3>	Issues Present: </h3>
+	<li>Lack of legislation </li>
+	<li>Lack of accountability </li>
+	<li>Lack of transparency and swiftness in dealing with cases </li>
+	<li>Lack of awareness</li>
+	<li>Not enough student organizations </li>
+<h3>	Objectives: </h3>
+	<li> Improve consent awareness at parties </li>
+	<li> Improve accessibility to CAPS and student services for sexual assault victims</li>
+	<li> Streamline the SVSH reporting system </li>
+<h3>Policy Proposals: </h3>
+	<li>Expand on greek life consent talk legislation by modifying the main consent talk template and have resources be advertised at parties based on research done through the Seed Grant Project. Currently only 57% of surveyed students feel that the current consent talk is effective, whereas 81% of students found a modified consent talk effective. We propose the ASUC mandates this new, revised consent talk be used at all Greek life and co-op parties.</li>
+	<li>Stricter penalty for individuals (be subject to ASUC created review boards that work through Panhellenic Council and Interfraternity Council instead of only greek life boards) </li>
+	<li>Better and more immediate counseling through CAPS (same as our mental health policy), and increasing priority for sexual assault victims. </li>
+	<li>Increase community support by creating more grant programs for prevention research </li>
+	<li>Increase funding for sexual safety oriented organizations, and incentivize the creation of more</li>
+</div>
+
+<style>
+	div {
+		padding-left: 2rem;
+		padding-right: 2rem;
+	}
+	h1 {
+		color: var(--pri);
+		font-size: 5vw;
+	}
+	h2 {
+		color: var(--pri);
+		font-size: 3vw;
+	}
+	p {
+		margin-top: 1rem;
+	}
+	h3 {
+		margin-top: 1rem;
+		font-size: 2vw;
+		color: var(--sec);
+		text-decoration-line: underline;
+	}
+	li {
+		margin-left: 1rem;
+	}
+</style>
